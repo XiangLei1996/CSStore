51849f263038ac3d5dfb659c153c8435e7b61c99
Frank <zxf_tech@outlook.com>
2019-05-07 17:22:46 +0800

    改了一些布局上的细节

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,11 @@ function toggleNavgation() {
     targetNav.classList.toggle("open");
 }
 
+// function closeNavgation() {
+//     var targetNav = document.getElementById("navbar-div");
+//     targetNav.classList.remove("open");
+// }
+
 function toggleIt(targetElement,statusClass) {
     if(targetElement.hasClass(statusClass)) {
         targetElement.removeClass(statusClass);
